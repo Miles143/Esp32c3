8d1dc72e14c0103b54934cb3d9fc0907fd1de59a
Miles143 <wonder_board.0k@icloud.com>
2025-02-26 18:11:58 -0500

    Update Blocks.js

--- apache-tomcat-11.0.0-M22/conf/Catalina/localhost/appClientModule/Blocks.js
+++ apache-tomcat-11.0.0-M22/conf/Catalina/localhost/appClientModule/Blocks.js
@@ -1,3 +1,18 @@
+import "HelloApp.js";
+
+{
+	int x, y;
+	x = 0xa1
+	y = 0xa2
+}if (x > 0x){
+	String s = "The value of x is " + x;
+	System.out.print(s);
+	}if (y > 0x){
+		String s "The value of y is " + y;
+		System.out.print(s);
+	}
+)
+// make scapy into file for Blocks to import //
 /**
  * 
  */
