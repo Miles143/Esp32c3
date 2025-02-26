ae1ba6c0c21eba757157dc046e09154af5aa7615
Miles143 <wonder_board.0k@icloud.com>
2025-02-26 17:51:59 -0500

    Update InitializingStrings.js

--- apache-tomcat-11.0.0-M22/conf/Catalina/localhost/appClientModule/InitializingStrings.js
+++ apache-tomcat-11.0.0-M22/conf/Catalina/localhost/appClientModule/InitializingStrings.js
@@ -1,3 +1,15 @@
+// Declaring String //
+
+import "HelloApp2.js";
+import "HelloWorld.js";
+import "ClassVariables.js";
+import "LocalVariables.js";
+import "InitializingVariables.js";
+
+String s;
+s = "Hello, World!";
+String x = " ";
+// Remember to disengage this scrpit if necessary. // 
 /**
  * 
  */
