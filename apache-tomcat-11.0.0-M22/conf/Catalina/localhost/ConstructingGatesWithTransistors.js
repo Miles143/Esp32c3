f6c6b72ff5c02b710b6ba37f53b38023d43cf026
Miles143 <wonder_board.0k@icloud.com>
2025-04-10 16:39:56 -0400

    ConstructingGatesWithTransistors

---
+++ apache-tomcat-11.0.0-M22/conf/Catalina/localhost/ConstructingGatesWithTransistors.js
@@ -0,0 +1,62 @@
+// Constructing Gates with Transistors //
+
+
+NOT Gate:
+
+Ex:
+         Source
+            |___V out
+         ___|
+V in____(    )
+		(____)
+            |
+		    |
+			≡
+		Ground
+
+NAND Gate:
+
+Ex:
+              		
+      Source
+            |___V out
+         ___|
+V 1 ____(    )
+		(____)
+            |
+		    |
+			| 
+		 ___|
+V 2 ____(    )
+		(____)
+	        |
+		    |
+            ≡   
+         Ground
+		 
+NOR Gate:
+    
+Ex:                      
+                      Source
+						 |
+             ____________|______________________V out
+			|                  			| 
+		 ___|                        ___|
+V 1 ____(    )               V 2____(    )
+        (____)                      (____)
+		    |                           |
+		    |                           |
+            ≡                           ≡
+         Ground                     Ground    
+                    
+                    
+                      
+                            
+
+
+
+
+		
+/**
+ * 
+ */
