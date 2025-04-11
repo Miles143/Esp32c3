2fa32f05876f6f595fb9d92b45253fc8f548ac8f
Miles143 <wonder_board.0k@icloud.com>
2025-04-11 17:56:52 -0400

    CircuitEquivalence

---
+++ apache-tomcat-11.0.0-M22/conf/Catalina/localhost/CircuitEquivalence.js
@@ -0,0 +1,40 @@
+// Circuit Equivalence //
+
+
+Ex:
+  Boolean Expression:
+  	Distributive Law:
+  		A(B+C) = AB + AC
+
+Logic Diagram:
+                       ___
+  A___________________(   )______A(B+C) 
+             ___     _(___)
+  B_________(   )___|                                                                           
+  C_________(___)    B+C                                                          
+                                                
+   Truth Table:
+   
+       A   B   C   B+C   A(B+C)
+  //   0   0   0    0     0
+  //   0   0   1    1     0
+  //   0   1   0    1     0
+  //   0   1   1    1     0
+  //   1   0   0    0     0
+  //   1   0   1    1     1
+  //   1   1   0    1     1
+  //   1   1   1    1     1 
+                                        
+                                                         
+
+
+
+
+
+
+
+
+
+/**
+ * 
+ */
