5508c8abc86112904029d6de0054fae570bf15bf
Miles143 <wonder_board.0k@icloud.com>
2025-04-11 18:09:28 -0400

    CircuitEquivalence

--- apache-tomcat-11.0.0-M22/conf/Catalina/localhost/CircuitEquivalence.js
+++ apache-tomcat-11.0.0-M22/conf/Catalina/localhost/CircuitEquivalence.js
@@ -24,7 +24,19 @@ Logic Diagram:
   //   1   0   1    1     1
   //   1   1   0    1     1
   //   1   1   1    1     1 
-                                        
+
+  PROPERTY			AND							  OR
+  Commutative	   AB =BA      				    A+B B+A
+  Associative		(AB)C = A(BC) + (AC)	     (A+B)+C = A+(B+C)
+  Distributive	   A(B+C) =  (AB) + (AC)		A+(BC) = (A+B)(A+C)
+  Identity		   A1 = A	        	        A + 0 = A
+  Complement	   A(A') = 0                    A +(A') = 1
+  DeMorgan's law    (AB)' = A' OR B'             (A+B)' = A'B'		
+  
+  
+  
+  
+                                          
 /**
  * 
  */
